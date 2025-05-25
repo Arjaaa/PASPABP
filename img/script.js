582eb573a978f6e3d1b88303da293e0243895890
Arjaaa <dhitoarza@gmail.com>
2025-05-25 19:13:46 +0700

    menambahkan logo dan fitur search

---
+++ img/script.js
@@ -0,0 +1,17 @@
+// function searchTokoh() {
+//   const input = document.getElementById("searchInput");
+//   const filter = input.value.toLowerCase();
+//   const container = document.querySelector(".container");
+//   const cards = container.getElementsByClassName("card");
+
+//   for (let i = 0; i < cards.length; i++) {
+//     const h2 = cards[i].getElementsByTagName("h2")[0];
+//     const textValue = h2.textContent || h2.innerText;
+
+//     if (textValue.toLowerCase().indexOf(filter) > -1) {
+//       cards[i].style.display = "";
+//     } else {
+//       cards[i].style.display = "none";
+//     }
+//   }
+// }
